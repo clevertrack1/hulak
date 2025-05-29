0e8d7d7a0519d8e304832231e1b8eeb15b7f83b2
paulclarke <paul.clarke@rfi.com.au>
2025-05-29 14:25:46 +0930

    Refactor `main.go` to delegate functionality to the `cmd` package and simplify execution flow

--- main.go
+++ main.go
@@ -2,52 +2,14 @@
 package main
 
 import (
-	"fmt"
-
-	userflags "github.com/xaaha/hulak/pkg/userFlags"
-	"github.com/xaaha/hulak/pkg/utils"
+	"github.com/xaaha/hulak/cmd"
 )
 
 func main() {
-	// Parse command line flags and subcmds
-	flags, err := userflags.ParseFlagsSubcmds()
-	if err != nil {
-		utils.PanicRedAndExit("%v", err)
-	}
+	// Assign the functions to the variables in the cmd package
+	cmd.InitializeProject = InitializeProject
+	cmd.HandleAPIRequests = HandleAPIRequests
 
-	// Extract flags
-	env := flags.Env
-	fp := flags.FilePath
-	fileName := flags.File
-	debug := flags.Debug
-	dir := flags.Dir
-	dirseq := flags.Dirseq
-
-	// Initialize project environment
-	envMap := InitializeProject(env)
-
-	// Which mode are we operating in
-	hasDirFlags := dir != "" || dirseq != ""
-	hasFileFlags := fp != "" || fileName != ""
-
-	var filePathList []string
-
-	if hasFileFlags {
-		filePathList, err = userflags.GenerateFilePathList(fileName, fp)
-		if err != nil {
-			// Only panic if no directory flags are provided
-			if !hasDirFlags {
-				utils.PanicRedAndExit("%v", err)
-			} else {
-				// When directory flags are present, just warn about the file flag error
-				utils.PrintWarning(fmt.Sprintf("Warning with file flags: %v", err))
-			}
-		}
-	}
-
-	if hasFileFlags || hasDirFlags {
-		HandleAPIRequests(envMap, debug, filePathList, dir, dirseq, fp)
-	} else {
-		utils.PrintWarning("No file or directory specified. Use -file, -fp, -dir, or -dirseq flags.")
-	}
+	// Execute the root command
+	cmd.Execute()
 }
